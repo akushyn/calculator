05be20da0c9087ae4a558229931ec1037c0edc49
Andrii Kushynskyi <akushyn@gmail.com>
2023-12-03 11:56:04 +0200

    implement operators API endpoint

---
+++ app/api/routes.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+
+from app.core.operators import OPERATORS
+
+router = APIRouter()
+
+
+@router.get("/operators", response_model=list[str])
+async def get_operators() -> list[str]:
+    return OPERATORS.keys()
