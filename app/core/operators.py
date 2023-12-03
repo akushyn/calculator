9dff001de580a0405c1e840d0cc0e5f4d34244d8
Andrii Kushynskyi <akushyn@gmail.com>
2023-12-03 11:55:18 +0200

    add operators abstraction

---
+++ app/core/operators.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractOperator(ABC):
+    @abstractmethod
+    def perform(self, x: float, y: float) -> float:
+        pass
+
+
+class AddOperator(AbstractOperator):
+    def perform(self, x: float, y: float) -> float:
+        return x + y
+
+
+class SubOperator(AbstractOperator):
+    def perform(self, x: float, y: float) -> float:
+        return x - y
+
+
+class MulOperator(AbstractOperator):
+    def perform(self, x: float, y: float) -> float:
+        return x * y
+
+
+class DivOperator(AbstractOperator):
+    def perform(self, x: float, y: float) -> float:
+        if y == 0:
+            raise ValueError("Division by zero")
+
+        return x / y
+
+
+OPERATORS = {
+    "+": AddOperator,
+    "-": SubOperator,
+    "*": MulOperator,
+    "/": DivOperator,
+}
