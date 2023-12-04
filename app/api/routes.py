dbb391c8b65f3765d539597136cbb3653d840206
Andrii Kushynskyi <akushyn@gmail.com>
2023-12-04 17:09:30 +0200

    update operators route

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 
-from app.core.operators import OPERATORS
+from app.core.operators import get_operators_keys
 
 router = APIRouter()
 
 
 @router.get("/operators", response_model=list[str])
-async def get_operators() -> list[str]:
-    return list(OPERATORS.keys())
+async def operators() -> list[str]:
+    return get_operators_keys()
