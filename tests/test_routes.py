1ae3bc8a927d60a2ba84dd0a8593d2ba8fe39343
Andrii Kushynskyi <akushyn@gmail.com>
2023-12-05 11:42:13 +0200

    add calculate api tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,9 +1,28 @@
+import pytest
+
 from app.core.operators import OPERATORS
+from app.models import CalculateRequest
 
 
-def test_get_operators(client):
-    url = "/api/operators"
+@pytest.fixture
+def calculate_request():
+    yield CalculateRequest(expression="2+3")
+
+
+@pytest.mark.asyncio
+async def test_get_operators(client):
+    url = "/api/operators/"
     response = client.get(url)
 
     assert response.status_code == 200
     assert response.json() == list(OPERATORS.keys())
+
+
+@pytest.mark.parametrize("colorize", [False, True])
+@pytest.mark.asyncio
+async def test_calculate(colorize, client, calculate_request):
+    calculate_request.colorize = colorize
+    response = client.post("/api/calculate/", json=calculate_request.model_dump())
+
+    assert response.status_code == 200
+    assert response.json() == {"result": 5.0, "color": None}
